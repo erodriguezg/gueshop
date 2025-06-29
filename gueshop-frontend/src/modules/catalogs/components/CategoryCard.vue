e734e509698713703217dc783146b404e8dbd9e6
Eduardo Rodriguez <erodriguez.cl@gmail.com>
2025-06-29 02:06:59 -0400

    creando catalogo categorias

--- gueshop-frontend/src/modules/catalogs/components/CategoryCard.vue
+++ gueshop-frontend/src/modules/catalogs/components/CategoryCard.vue
@@ -1,18 +1,28 @@
 <template>
     <div class="card bg-base-200 w-80 drop-shadow-xl">
         <figure>
-            <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+            <img :src="props.category.imageUrl ?? notFoundUrl" alt="Joyas" />
         </figure>
         <div class="card-body">
-            <h2 class="card-title">Card Title</h2>
-            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+            <h2 class="card-title">{{ props.category.name }}</h2>
+            <p> {{ props.category.description }}</p>
             <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
+                <button class="btn btn-primary">Ver</button>
             </div>
         </div>
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Category } from '../types/catalog';
+
+const notFoundUrl = 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp';
+
+const props = defineProps<{
+    category: Category
+}>()
+
+</script>
+
 
 <style scoped></style>
--- gueshop-frontend/src/modules/catalogs/views/CategoriesView.vue
+++ gueshop-frontend/src/modules/catalogs/views/CategoriesView.vue
@@ -1,12 +1,11 @@
 <template>
     <div class="grid grid-cols-3 gap-4 ml-4 mt-4">
-        <CatalogCard />
-        <CatalogCard />
-        <CatalogCard />
-        <CatalogCard />
-        <CatalogCard />
-        <CatalogCard />
-        <CatalogCard />
+        <CatalogCard v-for="n in 10" :key="n" 
+        :category="{ 
+            name: `Categoria ${n}`, 
+            description: `Very Long Description Number ${n}`, 
+            imageUrl: 'https://www.fmdos.cl/wp-content/uploads/2016/03/joyas.jpg',
+            slug: `category-n-${n}`}" />
     </div>
 </template>
 
